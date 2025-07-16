727c7a41f43b317909c9af6d5792c6d767b67818
Luca Orion <lucaorion@Lucas-MacBook-Pro.local>
2025-07-16 18:13:28 -0400

    Update encoder buttons to point to deployed Vercel app

--- encoder/lib/constants.ts
+++
@@ -1,5 +0,0 @@
-export const TILE = 1024; // px
-export const DEFAULT_BITS = 5; // LSBs (can be configured up to 8)
-export const MAX_BITS = 8; // Maximum LSBs we can use
-export const CHUNK = 8 << 20; // 8 MiB
-export const MAX_CANVAS = 16384; // browser PNG encoding limit
